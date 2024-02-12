978745d26af2d91572eab6dcaea0d6c9323424e1
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2024-02-12 19:29:05 +0300

    end practice

--- island.js
+++ island.js
@@ -11,6 +11,29 @@ function getNeighbors(row, col, graph) {
   // Return neighbors
 
   // Your code here
+  const neighbors = [];
+
+  if (row > 0 && graph[row - 1][col] === 1) {
+    const top = [row - 1, col];
+    neighbors.push(top);
+  }
+
+  if (col > 0 && graph[row][col - 1] === 1) {
+    const left = [row, col - 1];
+    neighbors.push(left);
+  }
+
+  if (row < graph.length - 1 && graph[row + 1][col] === 1) {
+    const bottom = [row + 1, col];
+    neighbors.push(bottom);
+  }
+
+  if (col < graph.length - 1 && graph[row][col + 1] === 1) {
+    const right = [row, col + 1];
+    neighbors.push(right);
+  }
+
+  return neighbors;
 }
 
 
@@ -38,6 +61,29 @@ function islandSize(row, col, graph) {
   // return size
 
   // Your code here
+  const start = [row, col];
+  const queue = [[start]];
+  const visited = new Set([start.join(',')]);
+  let size = 1;
+
+  while (queue.length > 0) {
+    const currentPath = queue.shift();
+    const lastInPath = currentPath[currentPath.length - 1];
+    const neighbors = getNeighbors(lastInPath[0], lastInPath[1], graph);
+
+    for (let i = 0; i < neighbors.length; i++) {
+      const neighbor = neighbors[i];
+
+      if (!visited.has(neighbor.join(','))) {
+        visited.add(neighbor.join(','));
+        const path = [...currentPath, neighbor];
+        queue.push(path);
+        size++
+      }
+    }
+  }
+
+  return size;
 }
 
 module.exports = [getNeighbors, islandSize];
